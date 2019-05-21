2ef94ab64db5b1c6254d236ee34ad3fc4db7e887
komisan19 <komiyama6219@gmail.com>
2019-05-22 05:08:06 +0900

    Add fileunit in docru

--- lib/docru/cli.rb
+++ lib/docru/cli.rb
@@ -5,6 +5,9 @@ module Docru
 	class CLI < Thor
 		dessc "make {Docru_string}", "make Dockerfile"
 		def make(str)
+			open('../docker/Dockerfile', 'w') do |f|
+				f.puts ""
+			end
 		end
 
 		desc "make_do {Docru_strin}}", "make docker-compose.yml"
