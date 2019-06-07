12a29cfc499dc6e6f12230e31356cccf2133df42
komisan19 <komiyama6219@gmail.com>
2019-06-07 20:32:22 +0900

    Add docker install

--- lib/otter/cli.rb
+++ lib/otter/cli.rb
@@ -22,5 +22,14 @@ module Otter
 			FileUtils.cp 'lib/otter/docker/compose/docker-compose.yml', '.'
 			puts true
 		end
+
+		desc "setup docker", "Install Docker & docker-compose"
+		def setup(docker)
+			system('apt-get install -y apt-transport-https ca-certificates curl software-properties-common &&\
+						 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - &&\
+						 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" &&\
+						 apt-get update &&\
+						 apt-get install -y docker-ce')
+		end
 	end
 end
